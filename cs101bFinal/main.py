b5dc7220ac469499a89b7f7fa33867e08fad2c24
Efren Haskell <efrenhask@gmail.com>
2023-12-01 15:55:20 -0500

    Added basic documentation

--- cs101bFinal/main.py
+++ cs101bFinal/main.py
@@ -2,9 +2,11 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, DataLoader
-# from net_utils import *
+from net_utils import *
+
 
 def load_data() -> (DataLoader, DataLoader):
+    # downloads MNIST training and test set and produces two data loaders
     train_set = torchvision.datasets.MNIST(root="data", train=True, transform=transforms.ToTensor(), download=True)
     test_set = torchvision.datasets.MNIST(root="data", train=False, transform=transforms.ToTensor(), download=True)
     train_loader: DataLoader = DataLoader(train_set, batch_size=100, shuffle=True)
@@ -13,6 +15,7 @@ def load_data() -> (DataLoader, DataLoader):
 
 
 def train(train_loader: DataLoader):
+    # loops over input and expected values sampled from train_loader
     for batch, data in enumerate(train_loader, 0):
         input_vals, expected = data
         pass
@@ -25,6 +28,7 @@ def test(test_loader: DataLoader):
 if __name__ == '__main__':
     epochs: int = 1
     train_data, test_data = load_data()
+    # outer loop for multiple training epochs
     for epoch in range(epochs):
         train(train_data)
         test(test_data)
