164ecfaae33ba2e42ea1fa8dc49a589064f59f54
samirrajani03 <112122615+samirrajani03@users.noreply.github.com>
2023-12-05 12:36:14 -0500

    Added train method

--- cs101bFinal/main.py
+++ cs101bFinal/main.py
@@ -1,8 +1,11 @@
-import torch
+import torch 
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, DataLoader
 from net_utils import *
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 
 def load_data() -> (DataLoader, DataLoader):
@@ -17,21 +20,37 @@ def load_data() -> (DataLoader, DataLoader):
     return train_loader, test_loader
 
 
-def train(train_loader: DataLoader):
+def train(train_loader: DataLoader, net, epoch_num):
     # loops over input and expected values sampled from train_loader
+    running_loss: float = 0.0
+    lossFunction = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), 0.001)
     for batch, data in enumerate(train_loader, 0):
         input_vals, expected = data
-        pass
+    
+    optimizer.zero_grad()
+
+    outputs = net(input_vals)
+    loss = lossFunction(outputs, expected)
+    loss.backward()
+    optimizer.step()
+
+    running_loss += loss.item()
+    if batch % 2000 == 1999:
+        print(f'[{epoch_num + 1}, {batch + 1:5d}] loss: {running_loss / 2000:.3f}')
+        running_loss = 0.0
+    print('Finished Training')
 
 
-def test(test_loader: DataLoader, epoch_num: int):
+def test(test_loader: DataLoader, net, epoch_num: int):
     pass
 
 
 if __name__ == '__main__':
     epochs: int = 1
     train_data, test_data = load_data()
+    net = Net()
     # outer loop for multiple training epochs
     for epoch in range(epochs):
-        train(train_data)
-        test(test_data, epoch)
+        train(train_data, net, epoch)
+        test(test_data, net, epoch)
